b2f8e16c76ae2ae90095c60b79e61939967fefc5
Amit <amit_bhambri@epam.com>
2025-05-26 17:22:09 +0530

    Initial

---
+++ src/test/java/com/example/SonarTest/SonarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SonarTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
